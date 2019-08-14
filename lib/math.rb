7c4c0aa3f28508334cb8ecc6014836a40d605f12
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-14 14:25:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,18 +5,28 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  num1 = 5
+  num2 = 10
   return num2 - num1
 end
 
 def division(num1, num2)
+  num1 = 2
+  num2 = 50
   return num2 / num1
 end
 
 def multiplication(num1, num2)
+  num1 = 4
+  num2 = 30
   return num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 = 5
+  num2 = 34
+  num3 = 
+  return 
 
 end
 
