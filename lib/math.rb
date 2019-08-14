10e2f379f7bc15693e65eef664ad54e91f56d58f
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-14 14:58:25 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+  num1 = 5
+  num2 = 4
+  return num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  num1 = 5
+  num2 = 10
+  return num2 - num1
 end
 
 def division(num1, num2)
-
+  num1 = 2
+  num2 = 50
+  return num2 / num1
 end
 
 def multiplication(num1, num2)
-
+  num1 = 4
+  num2 = 30
+  return num1 * num2
 end
 
 def modulo(num1, num2)
-
+  num1 = 5 
+  num2 = 34
+  return num2 % num1
 end
 
 def square_root(num)
-
+  num = 81
+  return Math.sqrt(num)
 end
